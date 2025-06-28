009b24cac64a16ad68f8ca1dd8663f3ec7759f58
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-29 00:59:52 +0600

    theme add

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router"
+import { ModeToggle } from "../mode-toggler"
 
 
 const Navbar = () => {
-  return (
-    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5 ">
-        <div className="flex items-center">
-            <span className="font-bold ml-2">Task</span>Master
-        </div>
-        <Link to="/tasks">Tasks</Link>
-        <Link to="/user">Users</Link>
-    </nav>
-  )
+    return (
+        <nav className="max-w-7xl mx-auto h-16 flex ju items-center justify-between gap-3 px-5 ">
+            <div className="flex items-center">
+                <span className="font-bold ml-2">Task</span>Master
+            </div>
+                <Link to="/tasks">Tasks</Link>
+                <Link to="/user">Users</Link>
+            <div className="ml-auto">
+                <ModeToggle></ModeToggle>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,12 @@ import {
   RouterProvider,
 } from "react-router";
 import router from './routes/index.tsx';
+import { ThemeProvider } from './providers/theme-provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
