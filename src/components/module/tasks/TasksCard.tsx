c61b94819a6af99232e406731e659c85494ff717
Khandaker-Mohyet <mohyet008@gmail.com>
2025-07-01 01:52:11 +0600

    add delete task

--- src/components/module/tasks/TasksCard.tsx
+++ src/components/module/tasks/TasksCard.tsx
@@ -27,7 +27,7 @@ const TasksCard = ({ task }: IProps) => {
                 </div>
                 <div className='flex gap-3 items-center'>
                     <Button
-                        onClick={}
+                        onClick={() => dispatch(deleteTask(task.id))}
                         variant="link" className='p-0 text-red-500'>
                         <Trash2 />
                     </Button>
